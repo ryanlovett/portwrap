a63c80a7eeda17275431f252fe782b0a6396f1f1
ryanlovett <rylo@berkeley.edu>
2023-01-04 22:37:39 -0800

    Format with black.

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ import setuptools
 
 setuptools.setup(
     name="portwrap",
-    version='0.0.1',
+    version="0.0.1",
     url="https://github.com/ryanlovett/portwrap",
     author="Ryan Lovett",
     author_email="rylo@berkeley.edu",
     description="Sandbox ports behind a proxy",
     packages=setuptools.find_packages(),
-	keywords=['bwrap', 'slirp4netns', 'nsenter'],
+    keywords=["bwrap", "slirp4netns", "nsenter"],
     entry_points={
-		'console_scripts': [
-			'portwrap = portwrap.__main__:main',
-		]
+        "console_scripts": [
+            "portwrap = portwrap.__main__:main",
+        ]
     },
 )
